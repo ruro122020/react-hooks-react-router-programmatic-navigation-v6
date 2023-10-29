e3bae501dce8fa8c7e7ca46beaa48e87e8d15842
Ruth <ruro122020@gmail.com>
2023-10-29 18:02:27 -0400

    add login functionality use useNavigate hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const navigate = useNavigate();
 
+  const login = ()=> {
+    setIsLoggedIn(true)
+  }
+  const logout = ()=>{
+    setIsLoggedIn(false)
+  }
 
+  useEffect(()=>{
+    if(isLoggedIn){
+      navigate('/')
+    }else{
+      navigate('/login')
+    }
+  }, [isLoggedIn])
   return (
     <div className="app">
-      <NavBar />
-      <Outlet />
+      <NavBar logout={logout}/>
+      <Outlet context={login}/>
     </div>
   );
 }
